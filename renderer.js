62d8c47b8d6ca917f907e3df90c2ad46f0de3e9d
barry.lavides <barry.lavides@.acommerce.asia>
2018-03-24 21:53:05 +0800

    - Browse websites - Back and forward button

--- renderer.js
+++ renderer.js
@@ -21,5 +21,37 @@ function showUrl (event) {
     omnibox.value = view.src;
 }
 
+function loadSiteUrl (event) {
+    // 13 is Enter key
+    if (event.keyCode === 13) {
+        // Remove focus
+        omnibox.blur();
+
+        let val = omnibox.value.toLowerCase();
+        var url = '';
+
+        if (val.indexOf('http') < 0) {
+            url = 'http://'+ val;
+        }
+
+        if (val.indexOf('.com') < 0) {
+            url = url + '.com'
+        }
+
+        view.loadURL(url)
+    }
+}
+
+function backView () {
+    view.goBack();
+}
+
+function forwardView () {
+    view.goForward();
+}
+
 refreshBtn.addEventListener('click', reloadView);
+omnibox.addEventListener('keydown', loadSiteUrl);
+backBtn.addEventListener('click', backView);
+forwardBtn.addEventListener('click', forwardView);
 view.addEventListener('did-finish-load', showUrl);
